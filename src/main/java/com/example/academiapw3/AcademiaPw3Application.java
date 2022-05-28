3eb59bfb34d487ba3302e68b87ed096ce2f76352
Renata Persch <renatapersch@hotmail.com>
2022-05-27 21:29:57 -0300

    Primeiro commit

---
+++ src/main/java/com/example/academiapw3/AcademiaPw3Application.java
@@ -0,0 +1,13 @@
+package com.example.academiapw3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaPw3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademiaPw3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/academiapw3/AcademiaPw3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.academiapw3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaPw3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
