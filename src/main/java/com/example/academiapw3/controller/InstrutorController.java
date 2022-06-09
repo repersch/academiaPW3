89124d76ed690f9a31e0113c777de7a5c22d8d29
João Domingues <jmmarao@gmail.com>
2022-06-08 21:34:01 -0300

    Corrigido controller e template de instrutor (listar e cadastrar)

--- src/main/java/com/example/academiapw3/controller/InstrutorController.java
+++ src/main/java/com/example/academiapw3/controller/InstrutorController.java
@@ -1,5 +1,6 @@
 package com.example.academiapw3.controller;
 
+import com.example.academiapw3.domain.Exercicio;
 import com.example.academiapw3.domain.Instrutor;
 import com.example.academiapw3.service.Instrutor.InstrutorServiceImpl;
 import org.springframework.http.HttpStatus;
@@ -11,31 +12,37 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.List;
 import java.util.Optional;
 
 @Controller
 @RequestMapping("/instrutores")
 public class InstrutorController {
-    private final InstrutorServiceImpl instrutorService;
+    private final InstrutorServiceImpl service;
     private final PasswordEncoder encoder;
 
-    public InstrutorController(InstrutorServiceImpl instrutorService, PasswordEncoder encoder) {
-        this.instrutorService = instrutorService;
+    public InstrutorController(InstrutorServiceImpl service, PasswordEncoder encoder) {
+        this.service = service;
         this.encoder = encoder;
     }
 
-//    @GetMapping("/listar")
-//    public ResponseEntity<List<Instrutor>> listarInstrutores() {
-//        return ResponseEntity.ok(instrutorService.buscarTodos());
-//    }
-
-    @GetMapping("/listar")
-    public String listarInstrutores(ModelMap modelMap) {
-        modelMap.addAttribute("instrutores", instrutorService.buscarTodos());
-        return "instrutor/lista";
+    @GetMapping("/cadastrar")
+    public String cadastrar(Model model){
+        model.addAttribute("instrutor", new Instrutor());
+        return "instrutor/cadastro";
     }
 
+    @PostMapping("/salvar")
+    public String salvar(Instrutor instrutor, RedirectAttributes attr){
+        service.salvar(instrutor);
+        attr.addFlashAttribute("success","Instrutor inserido com sucesso!");
+        return "redirect:/instrutores/listar";
+    }
+
+    @GetMapping("/listar")
+    public String listar(ModelMap modelMap) {
+        modelMap.addAttribute("instrutores", service.buscarTodos());
+        return "instrutor/lista";
+    }
 
 //    @PostMapping("/cadastrar")
 //    public ResponseEntity<Instrutor> salvarInstrutor(@RequestBody Instrutor instrutor) {
@@ -43,24 +50,24 @@ public class InstrutorController {
 //        return ResponseEntity.ok(instrutorService.salvar(instrutor));
 //    }
 
-    @GetMapping("/cadastrar")
-    public String cadastrar(Model model) {
-        model.addAttribute("instrutor", new Instrutor());
-        return "instrutor/cadastro";
-    }
+//    @GetMapping("/cadastrar")
+//    public String cadastrar(Model model) {
+//        model.addAttribute("instrutor", new Instrutor());
+//        return "instrutor/cadastro";
+//    }
 
-    @PostMapping("/salvar")
-    public String salvarInstrutor(Instrutor instrutor, RedirectAttributes attr) {
-        instrutor.setSenha(encoder.encode(instrutor.getSenha()));
-        instrutorService.salvar(instrutor);
-        attr.addFlashAttribute("success","Instrutor salvo com sucesso!");
-        return "redirect:/instrutores/listar";
-    }
+//    @PostMapping("/salvar")
+//    public String salvarInstrutor(Instrutor instrutor, RedirectAttributes attr) {
+//        instrutor.setSenha(encoder.encode(instrutor.getSenha()));
+//        service.salvar(instrutor);
+//        attr.addFlashAttribute("success", "Instrutor salvo com sucesso!");
+//        return "redirect:/instrutores/listar";
+//    }
 
     @GetMapping("validarSenha")
     public ResponseEntity<Boolean> validarSenha(@RequestParam String email,
                                                 @RequestParam String senha) {
-        Optional<Instrutor> optionalInstrutor = instrutorService.buscarPorEmail(email);
+        Optional<Instrutor> optionalInstrutor = service.buscarPorEmail(email);
 
         if (optionalInstrutor.isEmpty()) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(false);
